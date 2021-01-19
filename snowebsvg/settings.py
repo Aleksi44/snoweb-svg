b52495c728709f271914e85bc16153c49948d1b2
Alexis Le Baron <hello@snoweb.fr>
2021-01-19 09:35:53 +0100

    fix basedir

--- snowebsvg/settings.py
+++ snowebsvg/settings.py
@@ -2,7 +2,6 @@ import os
 import json
 import pkg_resources
 
-BASE_DIR = os.environ['BASE_DIR']
 BASE_URL_CSS = "https://static.snoweb.fr/snowebsvg/dist/css/"
 
 try:
@@ -10,6 +9,7 @@ try:
     VERSION = pkg_resources.get_distribution("snowebsvg").version
 except pkg_resources.DistributionNotFound:
     #  Develop part
+    BASE_DIR = os.environ['BASE_DIR']
     with open(os.path.join(BASE_DIR, 'package.json')) as package:
         data = json.load(package)
         VERSION = data['version']
